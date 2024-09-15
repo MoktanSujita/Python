99b29bc2f068825e13c59dccda56f18be2792a0c
Moktan Sujita <sujitamoktan77@gmail.com>
2024-09-15 23:39:30 +0545

    first commit

---
+++ rock-paper-scissors.py
@@ -0,0 +1,34 @@
+import random 
+
+def get_choices():
+  player_choice = input("enter a choice (rock, paper, scissors): ")
+  options = ["rock", "paper", "scissors"]
+  computer_choice = random.choice(options)
+  
+  choices = {"player": player_choice, "computer" : computer_choice}
+  return choices
+  
+def check_win(player, computer):
+  print(f"you chose {player} ,computer chose {computer}")
+  if player == computer:
+    return "It's a tie!" 
+  elif player == "rock"  :
+    if computer == "paper":
+      return "paper covers rock! you lose."
+    else:
+      return "rock smashes scissors! you win."
+  elif player == "paper":
+    if computer == "scissors":
+      return "scissors cuts paper! you lose."
+    else: 
+      return "paper covers rock! you win."
+  elif player == "scissors":
+      if computer == "rock":
+         return "rock smashes scissors! you lose."
+      else:
+         return "scissors cuts paper! you win."
+
+choices = get_choices()
+result = check_win(choices["player"], choices ["computer"])
+print(result)
+    
